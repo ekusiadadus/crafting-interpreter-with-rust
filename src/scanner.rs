29ac7ab58e590a895ed37c8f4c599f343b9fa881
ekusiadadus <ekusiadadus@gmail.com>
2024-04-28 15:03:38 +0900

    feat: add token type enum on scanner

---
+++ src/scanner.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Eq, PartialEq, Debug, Copy, Clone)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    Lambda,
+
+    Eof,
+}
